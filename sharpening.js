e5974a6e904fb7672f132af8e958e47d58fd8731
azuspan607 <aa.zuspan@gmail.com>
2020-10-30 23:07:05 -0700

    Add HPFA algorithm

--- sharpening.js
+++ sharpening.js
@@ -4,3 +4,4 @@ exports.simpleMean = require('users/aazuspan/geeSharpening:algorithms/simpleMean
 exports.brovey = require('users/aazuspan/geeSharpening:algorithms/brovey');
 exports.PCA = require('users/aazuspan/geeSharpening:algorithms/PCA');
 exports.IHS = require('users/aazuspan/geeSharpening:algorithms/IHS');
+exports.HPFA = require('users/aazuspan/geeSharpening:algorithms/HPFA');
