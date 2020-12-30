57f4b4861eafa48ad9eae682ebb830f11d28ae59
aa.zuspan <aa.zuspan@gmail.com>
2020-12-30 18:54:51 +0000

    Updated example.js (added 12 lines, deleted 12 lines).

--- example.js
+++ example.js
@@ -20,19 +20,19 @@ Map.centerObject(extent);
 // Select an example Landsat 8 TOA image
 var img = ee.Image("LANDSAT/LC08/C01/T1_TOA/LC08_047027_20160819").clip(extent);
 
-Export.image.toDrive({
-  image: img.select(),
-  description: "L8_sample",
-  scale: 30,
-  region: extent
-})
+// Export.image.toDrive({
+//   image: img.select(["B2", "B3", "B4"]),
+//   description: "L8_sample_vis",
+//   scale: 30,
+//   region: extent
+// })
 
-Export.image.toDrive({
-  image: img.select(),
-  description: "L8_sample",
-  scale: 30,
-  region: extent
-})
+// Export.image.toDrive({
+//   image: img.select("B8"),
+//   description: "L8_sample_pan",
+//   scale: 30,
+//   region: extent
+// })
 
 var vis = img.select(["B4", "B3", "B2"]);
 var pan = img.select(["B8"]);
