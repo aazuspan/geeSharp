564b67583c103a7914f5602892d2bed0603427ba
azuspan607 <aa.zuspan@gmail.com>
2022-01-19 21:43:00 -0800

    Remove PSNR because it cannot be accurately calculated in GEE (#2)

--- quality.js
+++ quality.js
@@ -2,7 +2,6 @@
 
 exports.MSE = require("users/aazuspan/geeSharp:qualityAlgorithms/MSE.js");
 exports.RMSE = require("users/aazuspan/geeSharp:qualityAlgorithms/RMSE.js");
-exports.PSNR = require("users/aazuspan/geeSharp:qualityAlgorithms/PSNR.js");
 exports.Q = require("users/aazuspan/geeSharp:qualityAlgorithms/Q.js");
 exports.bias = require("users/aazuspan/geeSharp:qualityAlgorithms/bias.js");
 exports.DIV = require("users/aazuspan/geeSharp:qualityAlgorithms/DIV.js");
